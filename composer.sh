5ae3a7ce7bfbdee8b2f461f99761f7ee3915d2a1
jairo <jairoramirez@emaginacion.com>
2024-05-11 02:32:27 +0000

    se agrego lo que necesita owncloud

--- composer.sh
+++ composer.sh
@@ -7,3 +7,5 @@ sudo apt update
 sudo apt-get update && sudo apt-get install php7.3 php7.3-mcrypt php7.3-xml php7.3-gd php7.3-opcache php7.3-mbstring
 sudo apt-get install php-mysql
 sudo apt-get install php7.3-mysql
+sudo apt-get install php-zip php-intl php-curl
+sudo apt-get install php7.3-zip php7.3-intl php7.3-curl
