b43dac0a7dca0ff5412a658eeaa045998f6f2c5d
jairo <jairoramirez@emaginacion.com>
2024-05-11 02:11:35 +0000

    cambios

--- composer.sh
+++ composer.sh
@@ -2,7 +2,10 @@ cd /tmp
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 cd /var/www/html
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
 sudo apt-get update && sudo apt-get install php7.3 php7.3-mcrypt php7.3-xml php7.3-gd php7.3-opcache php7.3-mbstring
-sudo composer install
 sudo apt-get install php-mysql
 sudo apt-get install php7.3-mysql
+sudo composer install
+
