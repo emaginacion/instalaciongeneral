39a96cec4069debf50aa6576e0f43f0654afb5b7
emaginacion <36830481+emaginacion@users.noreply.github.com>
2024-05-15 18:40:19 -0600

    para instalar docker compose

---
+++ jasper_docker/docker_basic_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
